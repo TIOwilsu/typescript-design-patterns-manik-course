1a5922989e523412885679b171118fd34abb8911
Wilson Queiroz Rocha <wilson.queiroz@outlook.com>
2023-12-05 22:24:21 -0300

    feat: :construction: import examples and exercises

---
+++ typescript/index.ts
@@ -0,0 +1,10 @@
+// Examples
+// import "./examples/abstraction";
+// import "./examples/encapsulation";
+// import "./examples/ocp";
+
+// Exercises
+// import "./exercises/abstraction";
+// import "./exercises/inheritance";
+// import "./exercises/liskov";
+import "./exercises/isp";
